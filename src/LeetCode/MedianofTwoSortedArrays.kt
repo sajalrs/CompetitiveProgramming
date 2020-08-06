9f82d231c3ed7e97501e125f5b3bb36001d28334
sajalrs <sajalsatyal@gmail.com>
2020-08-05 20:35:36 -0500

    MedianofTwoSortedArrays

---
+++ src/LeetCode/MedianofTwoSortedArrays.kt
@@ -0,0 +1,50 @@
+package LeetCode
+
+fun main(){
+
+}
+
+fun findMedianSortedArrays(nums1: IntArray, nums2:IntArray):Double{
+    var a = nums1
+    var b = nums2
+    var m = a.size
+    var n = b.size
+
+    if(m > n){
+        var temp = a
+        a = b
+        b = temp
+
+        var tmp = m
+        m = n
+        n = m
+    }
+
+    var iMin = 0
+    var iMax = m
+    var halfLen = (m+n+1)/2
+
+    while(iMin <= iMax){
+        var i = (iMin + iMax)/2
+        var j = halfLen - i
+            if(i < iMax && b[j-1] > a[i]){
+                iMin = i + 1
+            } else if(i > iMin && a[i-1]> b[j]) {
+                iMax = i - 1
+            }else {
+                var maxLeft = 0
+                if(i==0){maxLeft = b[j-1]}
+                else if(j==0){maxLeft = a[i-1]}
+                else {maxLeft = Math.max(a[i-1],b[j-1])}
+                if((m+n)%2 == 1) {return maxLeft.toDouble()}
+
+                var minRight = 0
+                if(i==m) {minRight = b[j]}
+                else if(j==n){minRight = a[i]}
+                else{minRight = Math.min(b[j], a[i])}
+
+                return (maxLeft+minRight)/2.0
+            }
+    }
+    return  0.0
+}
