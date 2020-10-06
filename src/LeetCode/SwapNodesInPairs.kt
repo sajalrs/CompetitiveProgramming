938dc068d2f8939b5ae8f219f20056378b7b9ce8
sajalrs <sajalsatyal@gmail.com>
2020-10-06 11:20:43 -0500

    Added SwapNodesInPairs

---
+++ src/LeetCode/SwapNodesInPairs.kt
@@ -0,0 +1,35 @@
+package LeetCode
+
+/**
+ * Example:
+ * var li = ListNode(5)
+ * var v = li.`val`
+ * Definition for singly-linked list.
+ */
+
+class ListNode(var `val`: Int) {
+    var next: ListNode? = null
+}
+
+fun swapPairs(orig: ListNode?): ListNode? {
+    var head = orig
+    var dummy = ListNode(-1)
+    dummy.next = head
+    var prevNode = dummy
+
+    while((head != null) && (head.next != null)){
+        var firstNode = head
+        var secondNode = head.next
+
+        prevNode.next = secondNode
+        firstNode.next = secondNode?.next
+        secondNode?.next = firstNode
+
+        prevNode = firstNode
+        head = firstNode.next
+
+    }
+
+    return dummy.next
+
+}
