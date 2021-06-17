36d3f20c738baa8de2f7dc3b756f7d2b43985892
sajalrs <sajalsatyal@gmail.com>
2021-06-16 19:21:44 -0500

    Largest Magic Square Working Finally

--- src/LeetCode/LargestMagicSquare.java
+++ src/LeetCode/LargestMagicSquare.java
@@ -2,54 +2,96 @@ package LeetCode;
 
 public class LargestMagicSquare {
     public int largestMagicSquare(int[][] grid) {
+        int M = grid.length;
+        int N = grid[0].length;
+        
+        int[][] cumRowSum = new int[M][N];
+        int[][] cumColSum = new int[M][N];
+        int[][] cumDiagSum = new int[M][N];
+        int[][] cumAntiDiagSum = new int[M][N];
 
-        int[][] rowSum = new int[grid.length + 2][grid[0].length+2];
-        int[][] colSum = new int[grid.length + 2][grid[0].length+2];
-        int[][] diagSum = new int[grid.length + 2][grid[0].length+2];
-        int[][] antiDiagSum = new int[grid.length + 2][grid[0].length+2];
-
-        for(int i =0; i<grid.length; i++){
-            for(int j=0; j<grid[0].length; j++){
-                rowSum[i+1][j+1] = rowSum[i+1][j] + grid[i][j];
-                colSum[i+1][j+1] = colSum[i][j+1] + grid[i][j];
-                diagSum[i+1][j+1] = diagSum[i][j] + grid[i][j];
-                antiDiagSum[i+1][j+1] = antiDiagSum[i][j+2] + grid[i][j];
+        for(int i=0; i< M; i++){
+            for(int j=0; j<N; j++){
+                if (j > 0) {
+                    cumRowSum[i][j] = cumRowSum[i][j - 1] + grid[i][j];
+                } else {
+                    cumRowSum[i][j] = grid[i][j];
+                }
+                if (i > 0) {
+                    cumColSum[i][j] = cumColSum[i - 1][j] + grid[i][j];
+                } else {
+                    cumColSum[i][j] = grid[i][j];
+                }
+                if (j > 0 && i > 0) {
+                    cumDiagSum[i][j] = cumDiagSum[i - 1][j - 1] + grid[i][j];
+                } else {
+                    cumDiagSum[i][j] = grid[i][j];
+                }
+                if (i > 0 && j < grid[0].length - 1) {
+                    cumAntiDiagSum[i][j] = cumAntiDiagSum[i - 1][j + 1] + grid[i][j];
+                } else {
+                    cumAntiDiagSum[i][j] = grid[i][j];
+                }
             }
         }
 
         int max = 1;
-        for(int i=2; i< Math.min(grid.length, grid[0].length); i++){
-            for(int j= 1; j < grid.length-1; j++){
-                for(int k=1; k<grid[0].length-1; k++){
+        for(int size = 2; size<= Math.min(M,N); size++) {
+            for(int i=0; i< M; i++){
+                for(int j =0; j< N; j++){
 
+                    if(i-size+1>=0 && j-size+1>=0){
 
+                        int iStart = i-size+1;
+                        int jStart = j-size+1;
 
+                        int rowSum = (cumRowSum[i][j] - cumRowSum[i][jStart]) + grid[i][jStart] ;
+                        int colSum = (cumColSum[i][j] - cumColSum[iStart][j]) + grid[iStart][j];
 
+                        if(rowSum != colSum){
+                            continue;
+                        }
 
+                        boolean isEqual = true;
+                        for(int k = iStart; k< i && isEqual; k++){
+                            int curRowSum = (cumRowSum[k][j] - cumRowSum[k][jStart]) + grid[k][jStart] ;
+                            if(curRowSum != rowSum){
+                                isEqual = false;
+                                break;
+                            }
+                        }
+
+                        for(int k = jStart; k< j && isEqual; k++){
+                            int curColSum = (cumColSum[i][k] - cumColSum[iStart][k]) + grid[iStart][k];
+                            if(curColSum != colSum){
+                                isEqual = false;
+                                break;
+                            }
+                        }
+
+                        if(!isEqual){
+                            continue;
+                        }
+
+                        int diagSum = (cumDiagSum[i][j] - cumDiagSum[iStart][jStart]) + grid[iStart][jStart];
+                        if(colSum != diagSum){
+                            continue;
+                        }
+
+                        int antiDiagSum = (cumAntiDiagSum[i][jStart] - cumAntiDiagSum[iStart][j]) + grid[iStart][j] ;
+                        if(antiDiagSum != diagSum){
+                            continue;
+                        }
+
+                        max = Math.max(size,max);
+                    }
                 }
             }
         }
 
 
-        System.out.println("Row Sum");
-        printInnerSquares(rowSum);
-        System.out.println("Col Sum");
-        printInnerSquares(colSum);
-        System.out.println("Diagonal Sum");
-        printInnerSquares(diagSum);
-        System.out.println("Anti Diagonal Sum");
-        printInnerSquares(antiDiagSum);
+        
+        return max;
 
-        return 0;
     }
-
-    public void printInnerSquares(int[][] grid){
-        for(int i= 1; i < grid.length-1; i++){
-            for(int j=1; j<grid[0].length-1; j++){
-                System.out.printf("%d ", grid[i][j]);
-            }
-            System.out.println();
-        }
-    }
-
 }
--- src/LeetCode/LargestMagicSquareTest.java
+++ src/LeetCode/LargestMagicSquareTest.java
@@ -17,8 +17,26 @@ public class LargestMagicSquareTest {
     public void test1(){
 
         int[][] array = new int[][]{new int[]{5,1,3,1},new int[]{9,3,3,1},new int[]{1,3,3,8}};
-        largestMagicSquare.largestMagicSquare(array);
 
+        assertEquals(2,largestMagicSquare.largestMagicSquare(array));
+    }
+
+    @Test
+    @DisplayName("Test 2")
+    public void test2(){
+
+        int[][] array = new int[][]{new int[]{7,1,4,5,6},new int[]{2,5,1,6,4},new int[]{1,5,4,3,2},new int[]{1,2,7,3,4}};
+
+        assertEquals(3,largestMagicSquare.largestMagicSquare(array));
+    }
+
+    @Test
+    @DisplayName("Test 3")
+    public void test3(){
+
+        int[][] array = new int[][]{new int[]{8,1,6},new int[]{3,5,7},new int[]{4,9,2},new int[]{7, 10,9}};
+
+        assertEquals(3,largestMagicSquare.largestMagicSquare(array));
     }
 
 }
