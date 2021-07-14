9c2a41cb1f06b2471224e6a3c8f01c4a516cd577
sajalrs <sajalsatyal@gmail.com>
2021-07-13 19:53:00 -0500

    Changed NearestExit to use bfs over backtracking

--- src/LeetCode/NearestExit.java
+++ src/LeetCode/NearestExit.java
@@ -1,52 +1,48 @@
 package LeetCode;
 
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
 public class NearestExit {
 
-    int minSteps = -1;
-
     public int nearestExit(char[][] maze, int[] entrance) {
-
-        maze[entrance[0]][entrance[1]] = '+';
-        backtrack(maze, new int[]{entrance[0] + 1, entrance[1]}, 1);
-        backtrack(maze, new int[]{entrance[0] - 1, entrance[1]}, 1);
-        backtrack(maze, new int[]{entrance[0], entrance[1] + 1}, 1);
-        backtrack(maze, new int[]{entrance[0], entrance[1] - 1}, 1);
-        return minSteps;
+       return dfs(maze, entrance);
     }
 
-    public void backtrack(char[][] maze, int[] curSpot, int steps) {
-        int x = curSpot[0];
-        int y = curSpot[1];
+    public int dfs(char[][] maze, int[] entrance) {
 
-        if (x > maze.length - 1 || x < 0 || y > maze[0].length - 1 || y < 0) {
-            return;
-        }
+        Queue<int[]> queue = new LinkedList<>();
+        queue.add(entrance);
+        maze[entrance[0]][entrance[1]] = '+';
+        int[][] delta = new int[][]{new int[]{1,0}, new int[]{-1,0}, new int[]{0,1}, new int[]{0,-1}};
+        int steps = 1;
 
-        if (maze[x][y] == '+') {
-            return;
-        }
+        while (!queue.isEmpty()){
+            int queueLength = queue.size();
+            for(int i=0; i<queueLength; i++){
 
-        if(minSteps != -1 && minSteps <= steps){
-            return;
-        }
+                int[] curSpot = queue.poll();
+                for(int j=0; j< delta.length; j++){
+                    int x = curSpot[0] + delta[j][0];
+                    int y = curSpot[1] + delta[j][1];
 
+                    if(x<maze.length && x>=0 && y<maze[0].length && y>=0 && maze[x][y] != '+'){
+                        if (x == 0 || x == maze.length - 1 || y == 0 || y == maze[0].length - 1) {
+                            return steps;
+                        }else {
+                            maze[x][y] = '+';
 
-        if (x == 0 || x == maze.length - 1 || y == 0 || y == maze[0].length - 1) {
-            if (minSteps == -1) {
-                minSteps = steps;
-            } else {
-                minSteps = Math.min(minSteps, steps);
+                            queue.add(new int[]{x,y});
+                        }
+                    }
+                }
             }
+            steps++;
         }
 
-        maze[x][y] = '+';
+        return -1;
 
-        backtrack(maze, new int[]{x + 1, y}, steps + 1);
-        backtrack(maze, new int[]{x - 1, y}, steps + 1);
-        backtrack(maze, new int[]{x, y + 1}, steps + 1);
-        backtrack(maze, new int[]{x, y - 1}, steps + 1);
-
-        maze[x][y] = '.';
 
 
     }
