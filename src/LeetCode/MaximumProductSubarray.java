7320122542a3f4b25cf57d1be3f38130e1585652
sajalrs <sajalsatyal@gmail.com>
2021-09-07 19:17:34 -0500

    Added Optimal Solution to MaximumProductSubarray

--- src/LeetCode/MaximumProductSubarray.java
+++ src/LeetCode/MaximumProductSubarray.java
@@ -1,44 +1,20 @@
 package LeetCode;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class MaximumProductSubarray {
     public int maxProduct(int[] nums) {
-        Map<Integer, Map<Integer,Integer>> productsOfSubarrays = new HashMap();
-        int maxProduct = nums[0];
-        for(int i = 0; i<nums.length;i++){
-            for(int j=0; j<nums.length; j++){
-                maxProduct = Math.max(maxProduct, productOfSubarray(i, j, productsOfSubarrays, nums));
-            }
+        int maxProd = nums[0];
+        int minProd = nums[0];
+        int max = nums[0];
+        for(int i = 1; i<nums.length;i++){
+            int choice1 = maxProd * nums[i];
+            int choice2 = minProd * nums[i];
+            maxProd = Math.max(nums[i],Math.max(choice1,choice2));
+            minProd = Math.min(nums[i],Math.min(choice1,choice2));
+            max = Math.max(max, maxProd);
         }
-        return maxProduct;
+
+        return max;
     }
 
-    public int productOfSubarray(int start, int end, Map<Integer,Map<Integer,Integer>> productsOfSubarrays, int[] nums){
-        if(start < 0 && end >= nums.length ) return 0;
 
-        if(productsOfSubarrays.containsKey(start) && productsOfSubarrays.get(start).containsKey(end)){
-            return productsOfSubarrays.get(start).get(end);
-        }
-
-        int toReturn;
-        if(start == end){
-            toReturn = nums[start];
-        } else if(start < end && start + 1 < nums.length){
-            toReturn = nums[start] * productOfSubarray(start+1, end, productsOfSubarrays, nums);
-        } else {
-            toReturn = 0;
-        }
-
-        if(productsOfSubarrays.containsKey(start)){
-            productsOfSubarrays.get(start).put(end,toReturn);
-        }else {
-            Map<Integer,Integer> toPut = new HashMap<>();
-            toPut.put(end, toReturn);
-            productsOfSubarrays.put(start, toPut);
-        }
-        return toReturn;
-
-    }
 }
