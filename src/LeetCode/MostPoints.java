954b9aad5d983259c2b1107db8039a7c3af66e7d
Sajal Satyal <sajalsatyal@gmail.com>
2022-01-17 23:00:23 -0600

    added most points and most points test

--- src/LeetCode/MostPoints.java
+++ src/LeetCode/MostPoints.java
@@ -1,2 +1,37 @@
-package LeetCode;public class MostPoints {
+package LeetCode;
+
+import java.util.*;
+
+public class MostPoints {
+    public long mostPoints(int[][] questions) {
+        Map<Integer, Map<Integer, Integer>> graph = new HashMap<>();
+        for(int i =0; i<questions.length; i++){
+            if(!graph.containsKey(i)){
+                graph.put(i, new HashMap<>());
+            }
+
+            Map<Integer, Integer> vertices = graph.get(i);
+
+            vertices.put(i+1, 0);
+            vertices.put(i+questions[i][1]+1, questions[i][0]);
+        }
+        Stack<int[]> stack = new Stack<>();
+        for(int i=0; i<questions.length; i++){
+            stack.add(new int[]{i,0});
+        }
+        int max = 0;
+        while (!stack.isEmpty()){
+
+            int[] curNode = stack.pop();
+            if(curNode[0] < questions.length){
+                Set<Integer> adjacentNodes = graph.get(curNode[0]).keySet();
+                for(int node: adjacentNodes){
+                    stack.add(new int[]{node, curNode[1] + graph.get(curNode[0]).get(node)});
+                }
+            } else {
+                max = Math.max(max, curNode[1]);
+            }
+        }
+        return max;
+    }
 }
--- src/LeetCode/MostPointsTest.java
+++ src/LeetCode/MostPointsTest.java
@@ -1,2 +1,41 @@
-package LeetCode;public class MostPointsTest {
+package LeetCode;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MostPointsTest {
+    MostPoints mostPoints;
+    @BeforeEach
+    public void setup(){
+        mostPoints = new MostPoints();
+    }
+
+    @Test
+    @DisplayName("Input: questions = [[3,2],[4,3],[4,4],[2,5]]\n" +
+            "Output: 5\n" +
+            "Explanation: The maximum points can be earned by solving questions 0 and 3.\n" +
+            "- Solve question 0: Earn 3 points, will be unable to solve the next 2 questions\n" +
+            "- Unable to solve questions 1 and 2\n" +
+            "- Solve question 3: Earn 2 points\n" +
+            "Total points earned: 3 + 2 = 5. There is no other way to earn 5 or more points.")
+    public void test1(){
+        assertEquals(5, mostPoints.mostPoints(new int[][]{new int[]{3,2},new int[]{4,3},new int[]{4,4},new int[]{2,5}}));
+    }
+
+    @Test
+    @DisplayName("Input: questions = [[1,1],[2,2],[3,3],[4,4],[5,5]]\n" +
+            "Output: 7\n" +
+            "Explanation: The maximum points can be earned by solving questions 1 and 4.\n" +
+            "- Skip question 0\n" +
+            "- Solve question 1: Earn 2 points, will be unable to solve the next 2 questions\n" +
+            "- Unable to solve questions 2 and 3\n" +
+            "- Solve question 4: Earn 5 points\n" +
+            "Total points earned: 2 + 5 = 7. There is no other way to earn 7 or more points.")
+    public void test2(){
+        assertEquals(7,mostPoints.mostPoints(new int[][]{new int[]{1,1},new int[]{2,2},new int[]{3,3},new int[]{4,4},new int[]{5,5}}));
+    }
+
 }
