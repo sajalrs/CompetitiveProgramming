686a4df8b17839d77062c4528113c3cf99ab48fd
Sajal Satyal <sajalsatyal@gmail.com>
2022-01-17 22:59:50 -0600

    added min moves and min moves test

--- src/LeetCode/MinMoves.java
+++ src/LeetCode/MinMoves.java
@@ -1,2 +1,21 @@
-package LeetCode;public class MinMoves {
+package LeetCode;
+
+public class MinMoves {
+    public int minMoves(int target, int maxDoubles) {
+        return recursion(target, maxDoubles);
+    }
+
+    private int recursion(int curNum, int maxDoubles){
+        if(curNum == 1){
+            return 0;
+        } else if(maxDoubles <= 0) {
+            return curNum-1;
+        } else {
+            if(curNum % 2 == 0){
+                return 1 + recursion(curNum/2, maxDoubles-1);
+            }else {
+                return 1+recursion(curNum-1, maxDoubles);
+            }
+        }
+    }
 }
