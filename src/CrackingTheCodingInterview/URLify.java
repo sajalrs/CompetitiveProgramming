dff8522c2a4d4f7b7424d667306ec249a9f6bc4a
Sajal Satyal <sajalsatyal@gmail.com>
2022-02-28 22:37:36 -0600

    added urlify and urlify test

--- src/CrackingTheCodingInterview/URLify.java
+++ src/CrackingTheCodingInterview/URLify.java
@@ -1,2 +1,54 @@
-package CrackingTheCodingInterview;public class URLify {
+package CrackingTheCodingInterview;
+
+import java.lang.reflect.Array;
+
+public class URLify {
+
+    public String urlify(String words) {
+        int noOfSpaces = 0;
+        for (int i = 0; i < words.length(); i++) {
+            if (words.charAt(i) == ' ') noOfSpaces++;
+        }
+
+        char[] charString = new char[words.length() + (2 * noOfSpaces)];
+        int i = 0;
+        for (; i < words.length(); i++) {
+            charString[i] = words.charAt(i);
+        }
+        for (; i < charString.length; i++) {
+            charString[i] = ' ';
+        }
+
+        return urlify(charString);
+    }
+
+    private String urlify(char[] words) {
+        int noOfSpaces = 0;
+        int i = words.length - 1;
+        while (words[i] == ' ') {
+            i--;
+        }
+        int curIndex = i;
+        for (; i > 0; i--) {
+            if (words[i] == ' ') noOfSpaces++;
+        }
+
+        while (noOfSpaces > 0) {
+            char curChar = words[curIndex];
+            if (curChar == ' ') {
+                noOfSpaces--;
+                words[curIndex + (2 * noOfSpaces)] = '%';
+                words[curIndex + (2 * noOfSpaces) + 1] = '2';
+                words[curIndex + (2 * noOfSpaces) + 2] = '0';
+
+            } else {
+                char temp = words[curIndex + (2 * noOfSpaces)];
+                words[curIndex + (2 * noOfSpaces)] = words[curIndex];
+                words[curIndex] = temp;
+            }
+            curIndex--;
+        }
+
+        return String.valueOf(words);
+    }
 }
--- src/CrackingTheCodingInterview/URLifyTest.java
+++ src/CrackingTheCodingInterview/URLifyTest.java
@@ -1,2 +1,29 @@
-package CrackingTheCodingInterview;public class URLifyTest {
+package CrackingTheCodingInterview;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class URLifyTest {
+
+    URLify urlify;
+    @BeforeEach
+    public void setup(){
+        urlify = new URLify();
+    }
+
+    @Test
+    @DisplayName("Input: \"sajal satyal\" Output: \"sajal%20satyal\"")
+    public void test1(){
+        assertEquals("sajal%20satyal", urlify.urlify("sajal satyal"));
+    }
+
+    @Test
+    @DisplayName("Input: \"sajal satyal is a big boy\" Output: \"sajal%20satyal%20is%20a%20big%20boy\"")
+    public void test2(){
+        assertEquals("sajal%20satyal%20is%20a%20big%20boy", urlify.urlify("sajal satyal is a big boy"));
+    }
+
 }
