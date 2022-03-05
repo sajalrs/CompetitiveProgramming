d78c16446472678374fd077338cbf5e53762460b
Sajal Satyal <sajalsatyal@gmail.com>
2022-03-05 14:47:02 -0600

    fixed errors in zero matrix

--- src/CrackingTheCodingInterview/ZeroMatrix.java
+++ src/CrackingTheCodingInterview/ZeroMatrix.java
@@ -35,6 +35,25 @@ public class ZeroMatrix {
     }
 
     public void zeroMatrixInplace(int[][] matrix){
+        boolean rowHasZero = false;
+        boolean colHasZero = false;
+
+
+        for(int j=0; j<matrix.length; j++){
+            if(matrix[0][j] == 0){
+                rowHasZero = true;
+                break;
+            }
+        }
+
+        for(int i=0; i<matrix.length; i++){
+            if(matrix[i][0] == 0){
+                colHasZero = true;
+                break;
+            }
+        }
+
+
         for(int i =1; i<matrix.length; i++){
             for(int j=1; j<matrix[i].length; j++){
                 int curNum = matrix[i][j];
@@ -46,26 +65,28 @@ public class ZeroMatrix {
             }
         }
 
-        for(int j=0; j<matrix.length; j++){
+        for(int j=1; j<matrix.length; j++){
             if(matrix[0][j] == 0) nullifyCol(matrix, j);
         }
 
 
-        for(int i=0; i<matrix.length; i++){
+        for(int i=1; i<matrix.length; i++){
             if(matrix[i][0] == 0) nullifyRow(matrix, i);
         }
 
+        if(rowHasZero) nullifyRow(matrix, 0);
+        if(colHasZero) nullifyCol(matrix, 0);
 
     }
 
     private void nullifyRow(int[][] matrix, int row){
-        for(int j=0; j<matrix[row].length; j++){
+        for(int j=1; j<matrix[row].length; j++){
             matrix[row][j] = 0;
         }
     }
 
     private void nullifyCol(int[][] matrix, int col){
-        for(int i=0; i<matrix.length; i++){
+        for(int i=1; i<matrix.length; i++){
             matrix[i][col] = 0;
         }
     }
