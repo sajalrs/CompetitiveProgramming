9bb09124fafa4929c3bf00eddc42ff1107c3a0b0
Sajal Satyal <sajalsatyal@gmail.com>
2022-03-05 13:45:49 -0600

    added rotate matrix and rotate matrix test

--- src/CrackingTheCodingInterview/RotateMatrix.java
+++ src/CrackingTheCodingInterview/RotateMatrix.java
@@ -1,2 +1,54 @@
-package CrackingTheCodingInterview;public class RotateMatrix {
+package CrackingTheCodingInterview;
+
+import java.util.Arrays;
+
+public class RotateMatrix {
+    public void rotateMatrix(int[][] matrix){
+        transposeMatrix(matrix);
+        swapColumns(matrix);
+    }
+
+    public void swapColumns(int[][] matrix){
+        for(int i=0;i<matrix.length; i++){
+            for(int j=0; j<matrix[0].length/2;j++){
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][matrix.length-j-1];
+                matrix[i][matrix.length-j-1] = temp;
+            }
+        }
+    }
+
+    public void transposeMatrix(int[][] matrix){
+        for(int i=0; i<matrix.length; i++){
+            for(int j=1; j+i<matrix.length; j++){
+                int temp = matrix[i+j][i];
+                matrix[i+j][i] = matrix[i][i+j];
+                matrix[i][i+j] = temp;
+            }
+        }
+    }
+
+    public static void main(String[] args){
+        RotateMatrix rotateMatrix = new RotateMatrix();
+        int[][] matrix = new int[][]{
+                new int[]{1,2,3},
+                new int[]{4,5,6},
+                new int[]{7,8,9}
+        };
+        rotateMatrix.transposeMatrix(matrix);
+
+        for(int[] row: matrix){
+            System.out.println(Arrays.toString(row));
+        }
+
+        int[][] matrix2 = new int[][]{
+                new int[]{5,1,9,11},new int[]{2,4,8,10},new int[]{13,3,6,7},new int[]{15,14,12,16}
+        };
+
+        rotateMatrix.transposeMatrix(matrix2);
+
+        for(int[] row: matrix2){
+            System.out.println(Arrays.toString(row));
+        }
+    }
 }
--- src/CrackingTheCodingInterview/RotateMatrixTest.java
+++ src/CrackingTheCodingInterview/RotateMatrixTest.java
@@ -1,2 +1,53 @@
-package CrackingTheCodingInterview;public class RotateMatrixTest {
+package CrackingTheCodingInterview;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+public class RotateMatrixTest {
+    RotateMatrix rotateMatrix;
+    @BeforeEach
+    public void setup(){
+        rotateMatrix = new RotateMatrix();
+    }
+
+    @Test
+    @DisplayName("Input: \n" +
+            "[1,2,3] \n" +
+            "[4,5,6] \n" +
+            "[7,8,9] \n" +
+            "Output: \n" +
+            "[7,4,1] \n" +
+            "[8,5,2] \n" +
+            "[9,6,3] \n")
+    public void test1(){
+        int[][] matrix = new int[][]{
+                new int[]{1,2,3},
+                new int[]{4,5,6},
+                new int[]{7,8,9}
+        };
+
+        rotateMatrix.rotateMatrix(matrix);
+
+        assertArrayEquals(new int[]{7,4,1}, matrix[0]);
+        assertArrayEquals(new int[]{8,5,2}, matrix[1]);
+        assertArrayEquals(new int[]{9,6,3}, matrix[2]);
+    }
+
+    @Test
+    @DisplayName("Input: matrix = [[5,1,9,11],[2,4,8,10],[13,3,6,7],[15,14,12,16]]\n" +
+            "Output: [[15,13,2,5],[14,3,4,1],[12,6,8,9],[16,7,10,11]]")
+    public void test2(){
+        int[][] matrix = new int[][]{
+                new int[]{5,1,9,11},new int[]{2,4,8,10},new int[]{13,3,6,7},new int[]{15,14,12,16}
+        };
+        rotateMatrix.rotateMatrix(matrix);
+        assertArrayEquals(new int[]{15,13,2,5}, matrix[0]);
+        assertArrayEquals(new int[]{14,3,4,1}, matrix[1]);
+        assertArrayEquals(new int[]{12,6,8,9}, matrix[2]);
+        assertArrayEquals(new int[]{16,7,10,11}, matrix[3]);
+    }
+
 }
