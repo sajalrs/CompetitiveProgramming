4c38bcf716809a15476578f1e12a369b3fe67d4c
Sajal Satyal <sajalsatyal@gmail.com>
2022-03-05 23:47:47 -0600

    added return kth to last" "

--- src/CrackingTheCodingInterview/ReturnKthToLast.java
+++ src/CrackingTheCodingInterview/ReturnKthToLast.java
@@ -1,2 +1,19 @@
-package CrackingTheCodingInterview;public class ReturnKthToLast {
+package CrackingTheCodingInterview;
+
+import LeetCode.ListNode;
+
+public class ReturnKthToLast {
+    public ListNode returnKthToLast(ListNode head, int k){
+        ListNode cur = head;
+        for (int i=0; i<k; i++){
+            head = head.next;
+        }
+
+        while (head!=null){
+            head = head.next;
+            cur = cur.next;
+        }
+
+        return cur;
+    }
 }
