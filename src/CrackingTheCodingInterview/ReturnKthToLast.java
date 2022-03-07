b01c65cc274f2256bc75dfc8347c8372008d8f0d
Sajal Satyal <sajalsatyal@gmail.com>
2022-03-07 00:29:05 -0600

    added return kth to last and test

--- src/CrackingTheCodingInterview/ReturnKthToLast.java
+++ src/CrackingTheCodingInterview/ReturnKthToLast.java
@@ -5,7 +5,7 @@ import LeetCode.ListNode;
 public class ReturnKthToLast {
     public ListNode returnKthToLast(ListNode head, int k){
         ListNode cur = head;
-        for (int i=0; i<k; i++){
+        for (int i=0; i<=k; i++){
             head = head.next;
         }
 
@@ -16,4 +16,61 @@ public class ReturnKthToLast {
 
         return cur;
     }
+
+    public ListNode returnKthToLastLengthKnown(ListNode head, int k){
+        ListNode cur = head;
+
+        int count = 0;
+        while(cur != null){
+            count++;
+            cur = cur.next;
+        }
+
+        for(int i=1; i<count-k; i++){
+            head = head.next;
+        }
+        return head;
+    }
+
+    public int returnKthToLastRecursivePrint(ListNode head, int k){
+        if(head == null){
+            return -1;
+        }
+
+        int index = returnKthToLastRecursivePrint(head.next, k) + 1;
+        if(index == k){
+            System.out.println(head.val);
+        }
+        return index;
+    }
+
+    private class Wrapper{
+        int index;
+        ListNode node;
+        public Wrapper(int index, ListNode node){
+            this.index = index;
+            this.node = node;
+        }
+    }
+
+    public ListNode returnKthToLastRecursive(ListNode head, int k){
+        return returnKthToLastRecursiveWrapper(head,k).node;
+    }
+
+    public Wrapper returnKthToLastRecursiveWrapper(ListNode head, int k){
+        if(head == null){
+            return new Wrapper(-1, null);
+        }
+
+        Wrapper wrapper = returnKthToLastRecursiveWrapper(head.next, k);
+        int index = wrapper.index + 1;
+        wrapper.index = index;
+        if(index == k){
+            wrapper.node = head;
+        }
+        return wrapper;
+    }
+
+
+
 }
--- src/CrackingTheCodingInterview/ReturnKthToLastTest.java
+++ src/CrackingTheCodingInterview/ReturnKthToLastTest.java
@@ -1,2 +1,73 @@
-package CrackingTheCodingInterview;public class ReturnKthToLastTest {
+package CrackingTheCodingInterview;
+
+import LeetCode.ListNode;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ReturnKthToLastTest {
+    ReturnKthToLast returnKthToLast;
+
+    @BeforeEach
+    public void setup(){
+        returnKthToLast = new ReturnKthToLast();
+    }
+
+    @Test
+    public void test1(){
+        assertEquals(5,returnKthToLast.returnKthToLast(arrayToList(new int[]{1,2,3,4,5}), 0).val);
+        assertEquals(4,returnKthToLast.returnKthToLast(arrayToList(new int[]{1,2,3,4,5}), 1).val);
+        assertEquals(1,returnKthToLast.returnKthToLast(arrayToList(new int[]{1,2,3,4,5}), 4).val);
+    }
+
+    @Test
+    public void test2(){
+        assertEquals(5,returnKthToLast.returnKthToLastLengthKnown(arrayToList(new int[]{1,2,3,4,5}), 0).val);
+        assertEquals(4,returnKthToLast.returnKthToLastLengthKnown(arrayToList(new int[]{1,2,3,4,5}), 1).val);
+        assertEquals(1,returnKthToLast.returnKthToLastLengthKnown(arrayToList(new int[]{1,2,3,4,5}), 4).val);
+    }
+
+    @Test
+    public void test3(){
+        returnKthToLast.returnKthToLastRecursivePrint(arrayToList(new int[]{1,2,3,4,5}), 0); //5
+        returnKthToLast.returnKthToLastRecursivePrint(arrayToList(new int[]{1,2,3,4,5}), 1); //4
+        returnKthToLast.returnKthToLastRecursivePrint(arrayToList(new int[]{1,2,3,4,5}), 4); //1
+    }
+
+    @Test
+    public void test4(){
+        assertEquals(5,returnKthToLast.returnKthToLastRecursive(arrayToList(new int[]{1,2,3,4,5}), 0).val);
+        assertEquals(4,returnKthToLast.returnKthToLastRecursive(arrayToList(new int[]{1,2,3,4,5}), 1).val);
+        assertEquals(1,returnKthToLast.returnKthToLastRecursive(arrayToList(new int[]{1,2,3,4,5}), 4).val);
+    }
+    private ListNode arrayToList(int[] array) {
+        ListNode cur = new ListNode(array[0]);
+        ListNode head = cur;
+        for (int i = 1; i < array.length; i++) {
+            ListNode prev = cur;
+            cur = new ListNode(array[i]);
+            prev.next = cur;
+        }
+        return head;
+    }
+
+    private int[] listToArray(ListNode listNode) {
+        ListNode cur = listNode;
+        int count = 0;
+        while (cur != null) {
+            count++;
+            cur = cur.next;
+        }
+        int[] arr = new int[count];
+        int index = 0;
+        cur = listNode;
+        while (cur != null) {
+            arr[index] = cur.val;
+            cur = cur.next;
+            index++;
+        }
+        return arr;
+    }
 }
