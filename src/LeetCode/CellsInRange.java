9c11e56a9653befdae79f46e217275f58859350a
Sajal Satyal <sajalsatyal@gmail.com>
2022-03-06 22:43:39 -0600

    added CellsInRange and CellsInRangeTest

--- src/LeetCode/CellsInRange.java
+++ src/LeetCode/CellsInRange.java
@@ -1,2 +1,25 @@
-package LeetCode;public class CellsInRange {
+package LeetCode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CellsInRange {
+    public List<String> cellsInRange(String s) {
+        String[] split = s.split(":");
+        String topLeft = split[0];
+        String bottomRight = split[1];
+        char colTopLeft = topLeft.charAt(0);
+        int rowTopLeft = Integer.parseInt(String.valueOf(topLeft.charAt(1)));
+        char colBottomRight = bottomRight.charAt(0);
+        int rowBottomRight = Integer.parseInt(String.valueOf(bottomRight.charAt(1)));
+
+        List<String> listOfCells = new ArrayList<>();
+        for (char i = colTopLeft; i <= colBottomRight; i++) {
+            for(int j=rowTopLeft; j<=rowBottomRight; j++){
+                String cell = i + String.valueOf(j);
+                listOfCells.add(cell);
+            }
+        }
+        return listOfCells;
+    }
 }
--- src/LeetCode/CellsInRangeTest.java
+++ src/LeetCode/CellsInRangeTest.java
@@ -1,2 +1,39 @@
-package LeetCode;public class CellsInRangeTest {
+package LeetCode;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+public class CellsInRangeTest {
+    CellsInRange cellsInRange;
+    @BeforeEach
+    public void setup(){
+        cellsInRange = new CellsInRange();
+    }
+
+    @Test
+    @DisplayName("Input: s = \"A1:F1\"\n" +
+            "Output: [\"A1\",\"B1\",\"C1\",\"D1\",\"E1\",\"F1\"]\n" +
+            "Explanation:\n" +
+            "The above diagram shows the cells which should be present in the list.\n" +
+            "The red arrow denotes the order in which the cells should be presented.")
+    public void test1(){
+        List<String> arr = cellsInRange.cellsInRange("A1:F1");
+        assertArrayEquals(new String[]{"A1","B1","C1","D1","E1","F1"},arr.toArray());
+    }
+
+    @Test
+    @DisplayName("Input: s = \"K1:L2\"\n" +
+            "Output: [\"K1\",\"K2\",\"L1\",\"L2\"]\n" +
+            "Explanation:\n" +
+            "The above diagram shows the cells which should be present in the list.\n" +
+            "The red arrows denote the order in which the cells should be presented.")
+    public void test2(){
+        List<String> arr = cellsInRange.cellsInRange("K1:L2");
+        assertArrayEquals(new String[]{"K1","K2","L1","L2"},arr.toArray());
+    }
 }
