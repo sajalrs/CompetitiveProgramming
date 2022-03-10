c2b977037ed3f0dd1501cdc7fc0d7405cf554259
Sajal Satyal <sajalsatyal@gmail.com>
2022-03-10 00:00:42 -0600

    added palindrome and palindrome test

--- src/CrackingTheCodingInterview/Palindrome.java
+++ src/CrackingTheCodingInterview/Palindrome.java
@@ -1,2 +1,106 @@
-package CrackingTheCodingInterview;public class Palindrome {
+package CrackingTheCodingInterview;
+
+import LeetCode.ListNode;
+
+import java.util.Stack;
+
+public class Palindrome {
+    public boolean palindrome(ListNode head){
+        ListNode runner = head;
+        Stack<Integer> stack = new Stack<>();
+        while (runner != null && runner.next != null){
+            stack.add(head.val);
+            runner = runner.next.next;
+            head = head.next;
+        }
+        if(runner!= null) {
+            head = head.next;
+        }
+        while (head !=null){
+            if(stack.pop() != head.val){
+                return false;
+            }
+            head = head.next;
+        }
+        return true;
+    }
+
+    public boolean palindromeReversed(ListNode head){
+        int count=0;
+        ListNode cur = head;
+        while (cur!=null){
+            count++;
+            cur = cur.next;
+        }
+
+        count = count/2;
+        ListNode reversed = reverse(head);
+        for(int i=0; i<count; i++){
+            if(head.val != reversed.val) return false;
+            head = head.next;
+            reversed = reversed.next;
+        }
+        return true;
+    }
+
+    public ListNode reverse(ListNode node){
+        ListNode head = null;
+        while (node != null){
+            ListNode newNode = new ListNode(node.val);
+            newNode.next = head;
+            head = newNode;
+            node = node.next;
+        }
+        return head;
+    }
+
+    public boolean palindromeRecursive(ListNode head){
+        int count=0;
+        ListNode cur = head;
+        while (cur!=null){
+            count++;
+            cur = cur.next;
+        }
+
+        Result result = new Result(null, false);
+        palindromeRecursiveHelper(count, head, result);
+        return result.isMatched;
+    }
+
+    private class Result{
+        ListNode node;
+        boolean isMatched;
+        public Result(ListNode node, boolean isMatched){
+            this.node = node;
+            this.isMatched = isMatched;
+        }
+    }
+
+    private void palindromeRecursiveHelper(int length, ListNode node, Result lastResult){
+        if(length != 0 && length != 1){
+            palindromeRecursiveHelper(length - 2, node.next, lastResult);
+        }
+
+        if(length == 0 || length == 1){
+            lastResult.node = length == 1 ? node.next : node ;
+            lastResult.isMatched = true;
+            return;
+        }
+
+        if(lastResult.isMatched == false){
+            lastResult.isMatched = false;
+            return;
+        }
+
+        if(node.val == lastResult.node.val){
+            lastResult.isMatched = true;
+            lastResult.node = lastResult.node.next;
+            return;
+        }else{
+            lastResult.isMatched = false;
+            return;
+        }
+    }
+
+
 }
--- src/CrackingTheCodingInterview/PalindromeTest.java
+++ src/CrackingTheCodingInterview/PalindromeTest.java
@@ -1,2 +1,124 @@
-package CrackingTheCodingInterview;public class PalindromeTest {
+package CrackingTheCodingInterview;
+
+import LeetCode.ListNode;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class PalindromeTest {
+    Palindrome palindrome;
+    @BeforeEach
+    public void setup(){
+        palindrome = new Palindrome();
+    }
+
+    @Test
+    public void test1(){
+        assertEquals(true, palindrome.palindrome(arrayToList(new int[]{1,0,1})));
+    }
+
+    @Test
+    public void test2(){
+        assertEquals(true, palindrome.palindrome(arrayToList(new int[]{1,1})));
+    }
+
+    @Test
+    public void test3(){
+        assertEquals(false, palindrome.palindrome(arrayToList(new int[]{1,1,0})));
+    }
+
+    @Test
+    public void test4(){
+        assertEquals(true, palindrome.palindromeReversed(arrayToList(new int[]{1,0,1})));
+    }
+
+    @Test
+    public void test5(){
+        assertEquals(true, palindrome.palindromeReversed(arrayToList(new int[]{1,1})));
+    }
+
+    @Test
+    public void test6() {
+        assertEquals(false, palindrome.palindromeReversed(arrayToList(new int[]{1, 1, 0})));
+    }
+
+    @Test
+    public void test7() {
+        assertEquals(true, palindrome.palindromeReversed(arrayToList(new int[]{})));
+    }
+
+    @Test
+    public void test8() {
+        assertEquals(true, palindrome.palindromeReversed(null));
+    }
+
+    @Test
+    public void test9(){
+        assertEquals(true, palindrome.palindromeRecursive(arrayToList(new int[]{1,0,1})));
+    }
+
+    @Test
+    public void test10(){
+        assertEquals(true, palindrome.palindromeRecursive(arrayToList(new int[]{1,1})));
+    }
+
+    @Test
+    public void test11(){
+        assertEquals(false, palindrome.palindromeRecursive(arrayToList(new int[]{1,1,0})));
+    }
+    @Test
+    public void test12(){
+        assertEquals(true, palindrome.palindromeRecursive(arrayToList(new int[]{1,2,3,3,2,1})));
+    }
+
+    @Test
+    public void test13(){
+        assertEquals(false, palindrome.palindromeRecursive(arrayToList(new int[]{1,2,3,3,2,1,12})));
+    }
+    @Test
+    public void test14(){
+        assertEquals(true, palindrome.palindromeRecursive(arrayToList(new int[]{1,2,3,4,3,2,1})));
+    }
+
+    @Test
+    public void test15(){
+        assertEquals(true, palindrome.palindrome(arrayToList(new int[]{1,2,3,3,2,1})));
+    }
+
+    @Test
+    public void test16(){
+        assertEquals(true, palindrome.palindromeReversed(arrayToList(new int[]{1,2,3,3,2,1})));
+    }
+
+    private ListNode arrayToList(int[] array) {
+        if(array.length == 0) return null;
+        ListNode cur = new ListNode(array[0]);
+        ListNode head = cur;
+        for (int i = 1; i < array.length; i++) {
+            ListNode prev = cur;
+            cur = new ListNode(array[i]);
+            prev.next = cur;
+        }
+        return head;
+    }
+
+    private int[] listToArray(ListNode listNode) {
+        if(listNode == null) return new int[]{};
+        ListNode cur = listNode;
+        int count = 0;
+        while (cur != null) {
+            count++;
+            cur = cur.next;
+        }
+        int[] arr = new int[count];
+        int index = 0;
+        cur = listNode;
+        while (cur != null) {
+            arr[index] = cur.val;
+            cur = cur.next;
+            index++;
+        }
+        return arr;
+    }
 }
