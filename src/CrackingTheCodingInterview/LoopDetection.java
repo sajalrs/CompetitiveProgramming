45f2e37829e73039f20314de14a2a103fe4ce48b
Sajal Satyal <sajalsatyal@gmail.com>
2022-03-10 23:35:47 -0600

    added loop detection and loop detection test

--- src/CrackingTheCodingInterview/LoopDetection.java
+++ src/CrackingTheCodingInterview/LoopDetection.java
@@ -1,2 +1,50 @@
-package CrackingTheCodingInterview;public class LoopDetection {
+package CrackingTheCodingInterview;
+
+import LeetCode.ListNode;
+
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class LoopDetection {
+
+    public ListNode loopDetection(ListNode node){
+        Set<ListNode> alreadyTraversed = new HashSet<>();
+        while (node != null) {
+            if(alreadyTraversed.contains(node)){
+                return node;
+            }else {
+                alreadyTraversed.add(node);
+            }
+            node = node.next;
+        }
+        return null;
+    }
+
+    public ListNode loopDetection2Runner(ListNode node){
+        ListNode slow = node;
+        ListNode fast = node;
+        slow = slow.next;
+        fast = fast.next.next;
+        while (slow != null && fast != null && fast.next != null){
+               if(slow == fast){
+                    return getStartingNode(slow, node);
+               }
+               slow = slow.next;
+               fast = fast.next.next;
+        }
+        return null;
+    }
+
+    private ListNode getStartingNode(ListNode slow, ListNode head){
+        ListNode node = head;
+        while (node != null && slow != null){
+            if(node == slow){
+                return node;
+            }
+            node = node.next;
+            slow = slow.next;
+        }
+        return null;
+    }
 }
--- src/CrackingTheCodingInterview/LoopDetectionTest.java
+++ src/CrackingTheCodingInterview/LoopDetectionTest.java
@@ -1,2 +1,124 @@
-package CrackingTheCodingInterview;public class LoopDetectionTest {
+package CrackingTheCodingInterview;
+
+import LeetCode.ListNode;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class LoopDetectionTest {
+    LoopDetection loopDetection;
+    @BeforeEach
+    public void setup(){
+        loopDetection = new LoopDetection();
+    }
+
+    @Test
+    public void test1(){
+        ListNode cur = arrayToList(new int[]{1,4,2,1,0});
+        ListNode temp = cur;
+        ListNode prev = cur;
+        cur = cur.next;
+
+        while (cur!=null){
+            prev = cur;
+            cur = cur.next;
+        }
+        prev.next = temp;
+        assertEquals(temp,loopDetection.loopDetection(temp));
+    }
+
+    @Test
+    public void test2(){
+        ListNode cur = arrayToList(new int[]{1,4,2,1,0});
+        ListNode temp = cur;
+        ListNode prev = cur;
+        cur = cur.next;
+
+        while (cur!=null){
+            prev = cur;
+            cur = cur.next;
+        }
+        assertEquals(null,loopDetection.loopDetection(temp));
+    }
+
+    @Test
+    public void test3(){
+        ListNode cur = arrayToList(new int[]{1,4,2,1,0});
+        ListNode temp = cur;
+        ListNode prev = cur;
+        cur = cur.next;
+
+        while (cur!=null){
+            prev = cur;
+            cur = cur.next;
+        }
+        assertEquals(null,loopDetection.loopDetection2Runner(temp));
+    }
+
+    @Test
+    public void test4(){
+        ListNode cur = arrayToList(new int[]{1,4,2,1,0});
+        ListNode temp = cur;
+        ListNode prev = cur;
+        cur = cur.next;
+
+        while (cur!=null){
+            prev = cur;
+            cur = cur.next;
+        }
+        prev.next = temp;
+        assertEquals(temp,loopDetection.loopDetection2Runner(temp));
+    }
+
+    @Test
+    public void test5(){
+        ListNode cur = arrayToList(new int[]{1,4,2,1,0});
+        ListNode head = cur;
+        ListNode prev = cur;
+        cur = cur.next;
+
+        for(int i = 0; i<2; i++){
+            prev = prev.next;
+            cur = cur.next;
+        }
+
+        ListNode temp = prev;
+
+        while (cur != null){
+            cur = cur.next;
+            prev = prev.next;
+        }
+        prev.next = temp;
+        assertEquals(temp,loopDetection.loopDetection2Runner(head));
+    }
+
+    private ListNode arrayToList(int[] array) {
+        ListNode cur = new ListNode(array[0]);
+        ListNode head = cur;
+        for (int i = 1; i < array.length; i++) {
+            ListNode prev = cur;
+            cur = new ListNode(array[i]);
+            prev.next = cur;
+        }
+        return head;
+    }
+
+    private int[] listToArray(ListNode listNode) {
+        ListNode cur = listNode;
+        int count = 0;
+        while (cur != null) {
+            count++;
+            cur = cur.next;
+        }
+        int[] arr = new int[count];
+        int index = 0;
+        cur = listNode;
+        while (cur != null) {
+            arr[index] = cur.val;
+            cur = cur.next;
+            index++;
+        }
+        return arr;
+    }
 }
