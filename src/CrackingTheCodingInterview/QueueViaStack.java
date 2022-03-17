5babad1699925bdf83270b96573938a395d3567c
Sajal Satyal <sajalsatyal@gmail.com>
2022-03-16 22:25:56 -0500

    added queue via stack and queue via stack tests

--- src/CrackingTheCodingInterview/QueueViaStack.java
+++ src/CrackingTheCodingInterview/QueueViaStack.java
@@ -1,2 +1,44 @@
-package CrackingTheCodingInterview;public class QueueViaStack {
+package CrackingTheCodingInterview;
+
+import java.util.Stack;
+
+public class QueueViaStack<T> implements Queue<T> {
+
+    Stack<T> stack1 = new Stack();
+    Stack<T> stack2 = new Stack();
+
+    @Override
+    public void add(T item) {
+        stack1.add(item);
+    }
+
+    @Override
+    public T remove() {
+        if(!stack2.isEmpty()){
+            return stack2.pop();
+        }
+
+        while (!stack1.isEmpty()){
+            stack2.add(stack1.pop());
+        }
+        return stack2.pop();
+    }
+
+    @Override
+    public T peek() {
+        if(!stack2.isEmpty()){
+            return stack2.peek();
+        }
+
+        while (!stack1.isEmpty()){
+            stack2.add(stack1.pop());
+        }
+        return stack2.peek();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return stack1.isEmpty() && stack2.isEmpty();
+    }
+
 }
--- src/CrackingTheCodingInterview/QueueViaStackTest.java
+++ src/CrackingTheCodingInterview/QueueViaStackTest.java
@@ -1,2 +1,51 @@
-package CrackingTheCodingInterview;public class QueueViaStackTest {
+package CrackingTheCodingInterview;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class QueueViaStackTest {
+    QueueViaStack<Integer> queueViaStack;
+
+    @BeforeEach
+    public void setup(){
+        queueViaStack = new QueueViaStack();
+    }
+
+    @Test
+    public void test1(){
+        Integer[] num = new Integer[]{1,2,3,4,5};
+        for(int number : num){
+            queueViaStack.add(number);
+        }
+
+        List<Integer> test = new ArrayList<>();
+        while (!queueViaStack.isEmpty()){
+            test.add(queueViaStack.remove());
+        }
+
+        assertArrayEquals(num, test.toArray());
+    }
+
+    @Test
+    public void test2(){
+        queueViaStack.add(0);
+        queueViaStack.add(2);
+        assertEquals(0, queueViaStack.remove());
+        queueViaStack.add(5);
+        assertEquals(2, queueViaStack.peek());
+        assertEquals(2, queueViaStack.remove());
+        queueViaStack.add(3);
+        queueViaStack.add(4);
+        assertEquals(5,queueViaStack.remove());
+        assertEquals(3,queueViaStack.remove());
+        assertEquals(4,queueViaStack.remove());
+    }
+
+
 }
