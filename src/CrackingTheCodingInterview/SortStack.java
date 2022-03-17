605852e3e3b79d10394526399857e93d78a1afd1
Sajal Satyal <sajalsatyal@gmail.com>
2022-03-16 22:26:28 -0500

    added sort stack and sort stack test

--- src/CrackingTheCodingInterview/SortStack.java
+++ src/CrackingTheCodingInterview/SortStack.java
@@ -1,2 +1,31 @@
-package CrackingTheCodingInterview;public class SortStack {
+package CrackingTheCodingInterview;
+import java.util.Stack;
+public class SortStack{
+
+    //Similar to tower of hanoi
+    public void sortStack(Stack<Integer> stack){
+        Stack<Integer> helperStack = new Stack<>();
+        while (!stack.isEmpty()){
+            helperStack.push(stack.pop());
+        }
+
+        while (!helperStack.isEmpty()){
+            if(stack.isEmpty()){
+                stack.push(helperStack.pop());
+            }
+            if(helperStack.peek() > stack.peek()){
+                int num = helperStack.pop();
+                while (!stack.isEmpty()){
+                    helperStack.push(stack.pop());
+                }
+                stack.push(num);
+                while (!helperStack.isEmpty() && helperStack.peek() < stack.peek()){
+                    stack.push(helperStack.pop());
+                }
+            }else {
+                stack.push(helperStack.pop());
+            }
+        }
+
+    }
 }
--- src/CrackingTheCodingInterview/SortStackTest.java
+++ src/CrackingTheCodingInterview/SortStackTest.java
@@ -1,2 +1,36 @@
-package CrackingTheCodingInterview;public class SortStackTest {
+package CrackingTheCodingInterview;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+public class SortStackTest {
+    SortStack sortStack;
+    @BeforeEach
+    public void setup(){
+        sortStack = new SortStack();
+    }
+
+    @Test
+    public void test1(){
+        Stack<Integer> stack = new Stack<>();
+        int[] num = new int[]{1,4,2,3,5};
+        for(int i=0; i<5; i++){
+            stack.push(num[i]);
+        }
+
+        sortStack.sortStack(stack);
+        List<Integer> sorted = new ArrayList<>();
+
+        while (!stack.isEmpty()){
+            sorted.add(stack.pop());
+        }
+
+        assertArrayEquals(new Integer[]{1,2,3,4,5}, sorted.toArray());
+    }
 }
