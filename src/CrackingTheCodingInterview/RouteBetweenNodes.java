68f94b8fe9c0f78f099cdc5ad23a2cebe9c7a7b6
Sajal Satyal <sajalsatyal@gmail.com>
2022-03-18 00:01:04 -0500

    added route between nodes and route between nodes test

--- src/CrackingTheCodingInterview/RouteBetweenNodes.java
+++ src/CrackingTheCodingInterview/RouteBetweenNodes.java
@@ -1,2 +1,35 @@
-package CrackingTheCodingInterview;public class RouteBetweenNodes {
+package CrackingTheCodingInterview;
+
+import java.util.Queue;
+import java.util.*;
+
+public class RouteBetweenNodes {
+
+    public boolean RouteBetweenNodes(Map<Integer, List<Integer>> graph, int node1, int node2) {
+       return bfs(graph, node1, node2);
+    }
+
+    private boolean bfs(Map<Integer, List<Integer>> graph, int start, int dest){
+        Queue<Integer> queue = new LinkedList<>();
+        Set<Integer> alreadyVisited = new HashSet<>();
+        queue.add(start);
+
+        while (!queue.isEmpty()){
+            int curNode = queue.poll();
+            if(!alreadyVisited.contains(curNode)){
+                for(int num: graph.get(curNode)){
+                    queue.add(num);
+                }
+
+                if(curNode == dest){
+                    return true;
+                }
+            }
+            alreadyVisited.add(curNode);
+        }
+
+        return false;
+    }
+
+
 }
--- src/CrackingTheCodingInterview/RouteBetweenNodesTest.java
+++ src/CrackingTheCodingInterview/RouteBetweenNodesTest.java
@@ -1,2 +1,44 @@
-package CrackingTheCodingInterview;public class RouteBetweenNodesTest {
+package CrackingTheCodingInterview;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.*;
+
+
+public class RouteBetweenNodesTest {
+    RouteBetweenNodes routeBetweenNodes;
+
+    @BeforeEach
+    public void setup() {
+        routeBetweenNodes = new RouteBetweenNodes();
+    }
+
+    @Test
+    public void test1() {
+        List<Integer> a = new LinkedList<>();
+        a.add(2);
+        List<Integer> b = new LinkedList<>();
+        b.add(1);
+
+        Map<Integer, List<Integer>> graph = new HashMap<>();
+        graph.put(1, a);
+        graph.put(2, b);
+
+        assert (routeBetweenNodes.RouteBetweenNodes(graph, 1, 2));
+    }
+
+    @Test
+    public void test2() {
+        List<Integer> a = new LinkedList<>();
+        a.add(3);
+        List<Integer> b = new LinkedList<>();
+        b.add(3);
+
+        Map<Integer, List<Integer>> graph = new HashMap<>();
+        graph.put(1, a);
+        graph.put(2, b);
+        graph.put(3, new LinkedList<>());
+        assert (!routeBetweenNodes.RouteBetweenNodes(graph, 1, 2));
+    }
 }
