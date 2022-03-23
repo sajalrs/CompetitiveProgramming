08b06f4d652164ce6ae406247252c585f6b39942
Sajal Satyal <sajalsatyal@gmail.com>
2022-03-22 21:44:12 -0500

    added successor and successor test

--- src/CrackingTheCodingInterview/Successor.java
+++ src/CrackingTheCodingInterview/Successor.java
@@ -1,2 +1,25 @@
-package CrackingTheCodingInterview;public class Successor {
+package CrackingTheCodingInterview;
+
+import Utility.TreeNode;
+
+public class Successor {
+    public TreeNode successor(TreeNode root, TreeNode key){
+        SuccessorNode successor = new SuccessorNode();
+        inorderTraversal(root, null, key, successor);
+        return successor.value;
+    }
+
+    private class SuccessorNode{
+        TreeNode value;
+    }
+
+    private void inorderTraversal(TreeNode node, TreeNode prev, TreeNode key, SuccessorNode successor){
+        if(node != null){
+            inorderTraversal(node.left, node, key, successor);
+            if(prev == key){
+                successor.value = node;
+            }
+            inorderTraversal(node.right, node, key, successor);
+        }
+    }
 }
--- src/CrackingTheCodingInterview/SuccessorTest.java
+++ src/CrackingTheCodingInterview/SuccessorTest.java
@@ -1,2 +1,28 @@
-package CrackingTheCodingInterview;public class SuccessorTest {
+package CrackingTheCodingInterview;
+
+import Utility.TreeNode;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SuccessorTest {
+    Successor successor;
+    @BeforeEach
+    public void setup(){
+        successor = new Successor();
+    }
+
+    @Test
+    public void test1(){
+        int[] arr = new int[]{1,2,3,4,5};
+        TreeNode root = new TreeNode(arr[0]);
+        root.left = new TreeNode(arr[1]);
+        root.right = new TreeNode(arr[2]);
+        TreeNode key = new TreeNode(arr[3]);
+        root.left.left = key;
+        TreeNode node = new TreeNode(arr[4]);
+        key.right = node;
+        assertEquals(node,successor.successor(root, key));
+    }
 }
