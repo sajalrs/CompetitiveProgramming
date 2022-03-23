79d6293263d0acb4536b58b6bd3a5006488176ac
Sajal Satyal <sajalsatyal@gmail.com>
2022-03-22 20:46:13 -0500

    added check balanced and check balanced test

--- src/CrackingTheCodingInterview/CheckBalanced.java
+++ src/CrackingTheCodingInterview/CheckBalanced.java
@@ -1,2 +1,51 @@
-package CrackingTheCodingInterview;public class CheckBalanced {
+package CrackingTheCodingInterview;
+
+import Utility.TreeNode;
+
+public class CheckBalanced {
+    private class HeightBalance {
+        int height = 0;
+        boolean isBalanced = true;
+
+        HeightBalance(int height, boolean isBalanced) {
+            this.height = height;
+            this.isBalanced = isBalanced;
+        }
+    }
+
+    public boolean checkBalanced(TreeNode root) {
+        return calculateHeight(root).isBalanced;
+    }
+
+    private HeightBalance calculateHeight(TreeNode node) {
+        if (node == null) {
+            return new HeightBalance(0, true);
+        }
+
+        HeightBalance heightBalanceLeft = calculateHeight(node.left);
+        HeightBalance heightBalanceRight = calculateHeight(node.right);
+
+        if (!heightBalanceLeft.isBalanced || !heightBalanceRight.isBalanced) return new HeightBalance(0, false);
+
+        int heightUnder = Math.max(heightBalanceLeft.height, heightBalanceRight.height);
+        boolean isBalanced = Math.abs(heightBalanceLeft.height - heightBalanceRight.height) <= 1;
+
+        return new HeightBalance(1 + heightUnder, isBalanced);
+    }
+
+    private int getHeight(TreeNode node) {
+        if (node == null) return -1;
+        return Math.max(getHeight(node.left), getHeight(node.right)) + 1;
+    }
+
+    public boolean checkBalancedN2(TreeNode root) {
+        if (root == null) return true;
+
+        int heightDiff = Math.abs(getHeight(root.left) - getHeight(root.right));
+        if (heightDiff > 1) {
+            return false;
+        } else {
+            return checkBalancedN2(root.left) && checkBalancedN2(root.right);
+        }
+    }
 }
--- src/CrackingTheCodingInterview/CheckBalancedTest.java
+++ src/CrackingTheCodingInterview/CheckBalancedTest.java
@@ -1,2 +1,61 @@
-package CrackingTheCodingInterview;public class CheckBalancedTest {
+package CrackingTheCodingInterview;
+
+import Utility.TreeNode;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CheckBalancedTest {
+    CheckBalanced checkBalanced;
+    @BeforeEach
+    public void setup(){
+        checkBalanced = new CheckBalanced();
+    }
+
+    @Test
+    public void test1(){
+        int[] arr = new int[]{1,2,3,4,5};
+        TreeNode root = new TreeNode(arr[0]);
+        TreeNode prev = root;
+        for(int i=1; i<arr.length; i++){
+            prev.left = new TreeNode(arr[i]);
+            prev = prev.left;
+        }
+        assertEquals(false, checkBalanced.checkBalanced(root));
+    }
+
+    @Test
+    public void test2(){
+        int[] arr = new int[]{1,2,3,4,5};
+        TreeNode root = new TreeNode(arr[0]);
+        root.left = new TreeNode(arr[1]);
+        root.right = new TreeNode(arr[2]);
+        root.left.left = new TreeNode(arr[3]);
+        root.right.right = new TreeNode(arr[4]);
+        assertEquals(true, checkBalanced.checkBalanced(root));
+    }
+
+    @Test
+    public void test3(){
+        int[] arr = new int[]{1,2,3,4,5};
+        TreeNode root = new TreeNode(arr[0]);
+        TreeNode prev = root;
+        for(int i=1; i<arr.length; i++){
+            prev.left = new TreeNode(arr[i]);
+            prev = prev.left;
+        }
+        assertEquals(false, checkBalanced.checkBalancedN2(root));
+    }
+
+    @Test
+    public void test4(){
+        int[] arr = new int[]{1,2,3,4,5};
+        TreeNode root = new TreeNode(arr[0]);
+        root.left = new TreeNode(arr[1]);
+        root.right = new TreeNode(arr[2]);
+        root.left.left = new TreeNode(arr[3]);
+        root.right.right = new TreeNode(arr[4]);
+        assertEquals(true, checkBalanced.checkBalancedN2(root));
+    }
 }
