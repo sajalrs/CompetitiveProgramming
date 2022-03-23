a6813076051aafa4d29a633b756afc85d7223b41
Sajal Satyal <sajalsatyal@gmail.com>
2022-03-22 21:19:17 -0500

    added validate bst and validate bst test

--- src/CrackingTheCodingInterview/ValidateBST.java
+++ src/CrackingTheCodingInterview/ValidateBST.java
@@ -1,2 +1,57 @@
-package CrackingTheCodingInterview;public class ValidateBST {
+package CrackingTheCodingInterview;
+
+import Utility.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ValidateBST {
+    public boolean validateBST(TreeNode root) {
+        if (root == null) return true;
+        Previous previous = new Previous();
+        return inorderTraversal(root, previous);
+    }
+
+    public boolean validateBSTNSpace(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        inorderTraversalNSpace(root, list);
+        for (int i = 1; i < list.size(); i++) {
+            if (list.get(i) <= list.get(i - 1)) return false;
+        }
+        return true;
+    }
+
+    private void inorderTraversalNSpace(TreeNode node, List<Integer> list) {
+        if (node != null) {
+            inorderTraversalNSpace(node.left, list);
+            list.add(node.val);
+            inorderTraversalNSpace(node.right, list);
+        }
+    }
+
+    private class Previous {
+        int value = Integer.MIN_VALUE;
+    }
+
+    private boolean inorderTraversal(TreeNode node, Previous previous) {
+        if (node != null) {
+            boolean left = inorderTraversal(node.left, previous);
+            if (node.val <= previous.value) return false;
+            previous.value = node.val;
+            boolean right = inorderTraversal(node.right, previous);
+            return left && right;
+        }
+        return true;
+    }
+
+    public boolean validateBSTMinMax(TreeNode root) {
+        return validateBSTMinMaxHelper(root, Integer.MIN_VALUE, Integer.MAX_VALUE);
+    }
+
+    private boolean validateBSTMinMaxHelper(TreeNode node, int min, int max) {
+        if (node == null) return true;
+        if (node.val < min || node.val > max) return false;
+        return validateBSTMinMaxHelper(node.left, min, node.val) &&
+                validateBSTMinMaxHelper(node.right, node.val, max);
+    }
 }
--- src/CrackingTheCodingInterview/ValidateBSTTest.java
+++ src/CrackingTheCodingInterview/ValidateBSTTest.java
@@ -1,2 +1,49 @@
-package CrackingTheCodingInterview;public class ValidateBSTTest {
+package CrackingTheCodingInterview;
+
+import Utility.TreeNode;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class ValidateBSTTest {
+    ValidateBST validateBST;
+    @BeforeEach
+    public void setup(){
+        validateBST = new ValidateBST();
+    }
+
+    @Test
+    public void test1(){
+        int[] arr = new int[]{2,1,3};
+        assert validateBST.validateBST(TreeNode.arrayToBinaryTree(arr));
+    }
+
+    @Test
+    public void test2(){
+        int[] arr = new int[]{1,2,3,4,5};
+        assert !validateBST.validateBST(TreeNode.arrayToBinaryTree(arr));
+    }
+
+    @Test
+    public void test3(){
+        int[] arr = new int[]{2,1,3};
+        assert validateBST.validateBSTNSpace(TreeNode.arrayToBinaryTree(arr));
+    }
+
+    @Test
+    public void test4(){
+        int[] arr = new int[]{1,2,3,4,5};
+        assert !validateBST.validateBSTNSpace(TreeNode.arrayToBinaryTree(arr));
+    }
+
+    @Test
+    public void test5(){
+        int[] arr = new int[]{2,1,3};
+        assert validateBST.validateBSTMinMax(TreeNode.arrayToBinaryTree(arr));
+    }
+
+    @Test
+    public void test6(){
+        int[] arr = new int[]{1,2,3,4,5};
+        assert !validateBST.validateBSTMinMax(TreeNode.arrayToBinaryTree(arr));
+    }
 }
