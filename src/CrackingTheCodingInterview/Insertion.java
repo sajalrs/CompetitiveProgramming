1e20a4555b1b861d272101a82f21bbc3067713c2
Sajal Satyal <sajalsatyal@gmail.com>
2022-04-10 22:19:32 -0500

    added insert digits into another

--- src/CrackingTheCodingInterview/Insertion.java
+++ src/CrackingTheCodingInterview/Insertion.java
@@ -1,2 +1,14 @@
-package CrackingTheCodingInterview;public class Insertion {
+package CrackingTheCodingInterview;
+
+public class Insertion {
+    public int insertion(int i, int j, int M, int N){
+        if(i>j|| i<0 || j>=32) return 0;
+        int allOnes = ~0;
+        int left = j < 31 ? (allOnes << j+1):0;
+        int right = ((1 << i) - 1);
+        int mask = left | right;
+        int M_shifted = M << i;
+        int N_cleared = mask & N;
+        return M_shifted | N_cleared;
+    }
 }
